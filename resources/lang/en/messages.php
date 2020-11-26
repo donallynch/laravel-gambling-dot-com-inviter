0ca9ac5b24d79cae0f2e4e686dbc34cbbb20a7da
donallynch <donal.lynch_msc@yahoo.ie>
2020-11-26 17:14:31 +0000

    Renamed project to Gambling.com (assignment had references to 'KaxMedia')

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'inviter' => 'KAXMedia Invite Sender',
+    'inviter' => 'Gambling.com Invite Sender',
     'inviter-desc' => 'These guys live within :threshold KM of our Dublin office and should be invited for some food and drinks:',
     'affiliate-id' => 'Affiliate ID: :id',
     'empty-list' => 'Empty list'
